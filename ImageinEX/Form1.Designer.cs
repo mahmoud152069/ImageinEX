4fcb5d829aee787ec2c0c198108b319dfddd4aba
Administrator <Administrator@DESKTOP-MTHT1H7>
2023-07-15 01:44:39 +0300

    Add project files.

---
+++ ImageinEX/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace ImageinEX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.browseFileButton = new System.Windows.Forms.Button();
+            this.filePathTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sheetComboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.columnComboBox = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.folderPathTextBox = new System.Windows.Forms.TextBox();
+            this.browseFolderButton = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // browseFileButton
+            // 
+            this.browseFileButton.Location = new System.Drawing.Point(12, 12);
+            this.browseFileButton.Name = "browseFileButton";
+            this.browseFileButton.Size = new System.Drawing.Size(120, 23);
+            this.browseFileButton.TabIndex = 0;
+            this.browseFileButton.Text = "Browse File";
+            this.browseFileButton.UseVisualStyleBackColor = true;
+            this.browseFileButton.Click += new System.EventHandler(this.browseFileButton_Click);
+            // 
+            // filePathTextBox
+            // 
+            this.filePathTextBox.Location = new System.Drawing.Point(138, 14);
+            this.filePathTextBox.Name = "filePathTextBox";
+            this.filePathTextBox.ReadOnly = true;
+            this.filePathTextBox.Size = new System.Drawing.Size(300, 20);
+            this.filePathTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Sheet Name";
+            // 
+            // sheetComboBox
+            // 
+            this.sheetComboBox.Enabled = false;
+            this.sheetComboBox.FormattingEnabled = true;
+            this.sheetComboBox.Location = new System.Drawing.Point(138, 48);
+            this.sheetComboBox.Name = "sheetComboBox";
+            this.sheetComboBox.Size = new System.Drawing.Size(300, 21);
+            this.sheetComboBox.TabIndex = 3;
+            this.sheetComboBox.SelectedIndexChanged += new System.EventHandler(this.sheetComboBox_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Column Name";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 147);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(776, 241);
+            this.dataGridView.TabIndex = 5;
+            // 
+            // columnComboBox
+            // 
+            this.columnComboBox.Enabled = false;
+            this.columnComboBox.FormattingEnabled = true;
+            this.columnComboBox.Location = new System.Drawing.Point(138, 88);
+            this.columnComboBox.Name = "columnComboBox";
+            this.columnComboBox.Size = new System.Drawing.Size(200, 21);
+            this.columnComboBox.TabIndex = 6;
+            this.columnComboBox.SelectedIndexChanged += new System.EventHandler(this.columnComboBox_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(135, 450);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Folder Path";
+            // 
+            // folderPathTextBox
+            // 
+            this.folderPathTextBox.Location = new System.Drawing.Point(138, 408);
+            this.folderPathTextBox.Name = "folderPathTextBox";
+            this.folderPathTextBox.ReadOnly = true;
+            this.folderPathTextBox.Size = new System.Drawing.Size(300, 20);
+            this.folderPathTextBox.TabIndex = 8;
+            // 
+            // browseFolderButton
+            // 
+            this.browseFolderButton.Location = new System.Drawing.Point(12, 405);
+            this.browseFolderButton.Name = "browseFolderButton";
+            this.browseFolderButton.Size = new System.Drawing.Size(120, 23);
+            this.browseFolderButton.TabIndex = 9;
+            this.browseFolderButton.Text = "Browse Folder";
+            this.browseFolderButton.UseVisualStyleBackColor = true;
+            this.browseFolderButton.Click += new System.EventHandler(this.browseFolderButton_Click);
+            // 
+            // searchButton
+            // 
+            this.searchButton.Enabled = false;
+            this.searchButton.Location = new System.Drawing.Point(469, 88);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(120, 23);
+            this.searchButton.TabIndex = 10;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Enabled = false;
+            this.saveButton.Location = new System.Drawing.Point(668, 450);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(120, 23);
+            this.saveButton.TabIndex = 11;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 485);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.browseFolderButton);
+            this.Controls.Add(this.folderPathTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.columnComboBox);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.sheetComboBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.filePathTextBox);
+            this.Controls.Add(this.browseFileButton);
+            this.Name = "Form1";
+            this.Text = "ImageinEX";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button browseFileButton;
+        private System.Windows.Forms.TextBox filePathTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox sheetComboBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.ComboBox columnComboBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox folderPathTextBox;
+        private System.Windows.Forms.Button browseFolderButton;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Button saveButton;
+    }
+}
---
+++ ImageinEX/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using OfficeOpenXml;
+using LicenseContext = OfficeOpenXml.LicenseContext;
+
+namespace ImageinEX
+{
+    public partial class Form1 : Form
+    {
+        private string selectedFilePath = string.Empty;
+        private string selectedSheetName = string.Empty;
+        private string selectedColumnName = string.Empty;
+        private string selectedFolderPath = string.Empty;
+        private DataTable dataTable;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Initialize the dropdown list of sheet names
+            sheetComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+            sheetComboBox.Enabled = false;
+
+            // Initialize the DataGridView
+            dataTable = new DataTable();
+            dataGridView.DataSource = dataTable;
+        }
+
+        private void browseFileButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Excel Files|*.xlsx;*.xls";
+            openFileDialog.Title = "اختر ملف الإكسل";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                selectedFilePath = openFileDialog.FileName;
+                filePathTextBox.Text = selectedFilePath;
+                LoadSheetNames(selectedFilePath);
+            }
+        }
+
+        private void LoadSheetNames(string filePath)
+        {
+            sheetComboBox.Items.Clear();
+            columnComboBox.Items.Clear();
+
+            using (var package = new ExcelPackage(new FileInfo(filePath)))
+            {
+                var worksheets = package.Workbook.Worksheets;
+                foreach (var worksheet in worksheets)
+                {
+                    sheetComboBox.Items.Add(worksheet.Name);
+                }
+            }
+
+            if (sheetComboBox.Items.Count > 0)
+            {
+                sheetComboBox.Enabled = true;
+                sheetComboBox.SelectedIndex = 0;
+                columnComboBox.Enabled = true;
+                searchButton.Enabled = true;
+            }
+            else
+            {
+                sheetComboBox.Enabled = false;
+                columnComboBox.Enabled = false;
+                searchButton.Enabled = false;
+            }
+        }
+
+        private void sheetComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedSheetName = sheetComboBox.SelectedItem.ToString();
+            UpdateDataTable();
+            LoadColumnNames(selectedSheetName);
+            UpdateDataGridView();
+        }
+
+        private void UpdateDataTable()
+        {
+            dataTable.Clear();
+
+            using (ExcelPackage package = new ExcelPackage(new FileInfo(selectedFilePath)))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[selectedSheetName];
+
+                int rows = worksheet.Dimension.Rows;
+                int columns = worksheet.Dimension.Columns;
+
+                for (int i = 1; i <= columns; i++)
+                {
+                    string columnName = worksheet.Cells[1, i].Value?.ToString();
+                    if (!dataTable.Columns.Contains(columnName))
+                    {
+                        dataTable.Columns.Add(columnName);
+                    }
+                }
+
+                for (int row = 2; row <= rows; row++)
+                {
+                    DataRow dataRow = dataTable.NewRow();
+                    for (int col = 1; col <= columns; col++)
+                    {
+                        string columnName = worksheet.Cells[1, col].Value?.ToString();
+                        string cellValue = worksheet.Cells[row, col].Value?.ToString();
+
+                        if (dataTable.Columns.Contains(columnName))
+                        {
+                            dataRow[columnName] = cellValue;
+                        }
+                    }
+                    dataTable.Rows.Add(dataRow);
+                }
+            }
+        }
+
+        private void LoadColumnNames(string sheetName)
+        {
+            columnComboBox.Items.Clear();
+
+            using (ExcelPackage package = new ExcelPackage(new FileInfo(selectedFilePath)))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[sheetName];
+
+                int columns = worksheet.Dimension.Columns;
+
+                for (int col = 1; col <= columns; col++)
+                {
+                    string columnName = worksheet.Cells[1, col].Value?.ToString();
+                    columnComboBox.Items.Add(columnName);
+                }
+            }
+        }
+
+        private void columnComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedColumnName = columnComboBox.SelectedItem?.ToString();
+            UpdateDataGridView();
+        }
+
+        private void UpdateDataGridView()
+        {
+            if (!string.IsNullOrEmpty(selectedColumnName))
+            {
+                DataView dataView = dataTable.DefaultView;
+                dataView.RowFilter = $"[{selectedColumnName}] <> ''";
+                dataGridView.DataSource = dataView;
+            }
+            else
+            {
+                dataGridView.DataSource = dataTable;
+            }
+        }
+
+        private void browseFolderButton_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    selectedFolderPath = folderBrowserDialog.SelectedPath;
+                    folderPathTextBox.Text = selectedFolderPath;
+                }
+            }
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(selectedColumnName) || string.IsNullOrEmpty(selectedFolderPath))
+            {
+                MessageBox.Show("Please select a column and a folder path.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            DataTable newDataTable = dataTable.Clone();
+            newDataTable.Columns.Add("IMAGE", typeof(string));
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                string cellValue = row[selectedColumnName].ToString();
+                List<string> matchingFolders = Directory.GetDirectories(selectedFolderPath, $"{cellValue}*")
+                                                        .Select(folderPath => new DirectoryInfo(folderPath).Name)
+                                                        .ToList();
+
+                if (matchingFolders.Count > 0)
+                {
+                    foreach (string folderName in matchingFolders)
+                    {
+                        DataRow newRow = newDataTable.NewRow();
+                        newRow.ItemArray = row.ItemArray;
+                        newRow["IMAGE"] = folderName;
+                        newDataTable.Rows.Add(newRow);
+                    }
+                }
+                else
+                {
+                    DataRow newRow = newDataTable.NewRow();
+                    newRow.ItemArray = row.ItemArray;
+                    newRow["IMAGE"] = "";
+                    newDataTable.Rows.Add(newRow);
+                }
+            }
+
+            dataTable = newDataTable;
+            UpdateDataGridView();
+            saveButton.Enabled = true;
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (dataTable.Columns.Contains("IMAGE"))
+            {
+                using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+                {
+                    saveFileDialog.Filter = "Excel Files|*.xlsx";
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        FileInfo fileInfo = new FileInfo(saveFileDialog.FileName);
+                        using (ExcelPackage package = new ExcelPackage(fileInfo))
+                        {
+                            ExcelWorksheet worksheet = package.Workbook.Worksheets.Add(selectedSheetName);
+
+                            int rows = dataTable.Rows.Count;
+                            int columns = dataTable.Columns.Count;
+
+                            for (int col = 1; col <= columns; col++)
+                            {
+                                worksheet.Cells[1, col].Value = dataTable.Columns[col - 1].ColumnName;
+                            }
+
+                            for (int row = 2; row <= rows + 1; row++)
+                            {
+                                for (int col = 1; col <= columns; col++)
+                                {
+                                    worksheet.Cells[row, col].Value = dataTable.Rows[row - 2][col - 1].ToString();
+                                }
+                            }
+
+                            package.Save();
+                            MessageBox.Show("Table saved successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("There is no IMAGE column to save.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
